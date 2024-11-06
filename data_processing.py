07e271cf6d7022596a1bc3ce527d9307569653d2
Tat TANPRASERT <tat.t@ku.th>
2024-11-06 10:12:15 +0700

    Add procedural style code

--- data_processing.py
+++ data_processing.py
@@ -35,20 +35,48 @@ print()
 
 # Print the average temperature for all the cities in Italy
 # Write code for me
+temps = []
+my_country = 'Italy'
+for city in cities:
+    if city['country'] == my_country:
+        temps.append(float(city['temperature']))
+print("The average temperature of all the cities in", my_country, ":")
+print(sum(temps)/len(temps))
+print()
 
 # Print the max temperature for all the cities in Italy
 # Write code for me
+temps = []
+my_country = 'Italy'
+for city in cities:
+    if city['country'] == my_country:
+        temps.append(float(city['temperature']))
+print("The max temperature of all the cities in", my_country, ":")
+print(max(temps))
+print()
 
 # Let's write a function to filter out only items that meet the condition
 def filter(condition, dict_list):
-    pass
+    filtered_list = []
+    for item in dict_list:
+        if condition(item):
+            filtered_list.append(item)
+    return filtered_list
+
 
 # Let's write a function to do aggregation given an aggregation function and an aggregation key
 def aggregate(aggregation_key, aggregation_function, dict_list):
-    pass
+    agg_list = [float(x[aggregation_key]) for x in dict_list]
+    return aggregation_function(agg_list)
+
+
 
 # Let's write code to
 # - print the average temperature for all the cities in Italy
+print(aggregate('temperature',lambda x: sum(x)/len(x),(filter(lambda x: x['country'] == 'Italy', cities))))
 # - print the average temperature for all the cities in Sweden
+print(aggregate('temperature',lambda x: sum(x)/len(x),(filter(lambda x: x['country'] == 'Sweden', cities))))
 # - print the min temperature for all the cities in Italy
+print(aggregate('temperature',lambda x: min(x),(filter(lambda x: x['country'] == 'Italy', cities))))
 # - print the max temperature for all the cities in Sweden
+print(aggregate('temperature',lambda x: max(x),(filter(lambda x: x['country'] == 'Sweden', cities))))
