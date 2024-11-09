0abd4980c6872835a6ef541d9da76abece2bb53a
Tat TANPRASERT <tat.t@ku.th>
2024-11-09 21:29:58 +0700

    Add oop style code

--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,6 @@
 import csv, os
 
+
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
@@ -15,12 +16,59 @@ with open(os.path.join(__location__, 'Countries.csv')) as f:
     for r in rows:
         countries.append(dict(r))
 
+class TableDB:
+    def __init__(self):
+        self.table_database = []
+
+    def insert(self,table):
+        self.table_database.append(table)
+
+    def search(self,table_name):
+        for table in self.table_database:
+            if table.table_name == table_name:
+                return table
+
+class Table:
+    def __init__(self,table_name,table):
+        self.table_name = table_name
+        self.table = table
+
+    def filter(self,condition):
+        filtered_list = []
+        for item in self.table:
+            if condition(item):
+                filtered_list.append(item)
+        return Table(None,filtered_list)
+
+    def aggregate(self,aggregation_function,aggregation_key):
+        agg_list = [float(x[aggregation_key]) for x in self.table]
+        return aggregation_function(agg_list)
+
+    def __str__(self):
+        text = ''
+        for key in self.table[0].keys():
+            text = text + f'{key:<25}'.upper()
+        text = text + f'\n'
+        for item in self.table:
+            for value in item.values():
+                text = text + f'{value:<25}'
+            text = text + f'\n'
+        return text
+
+city = Table('city',cities)
+country = Table('country',countries)
+database = TableDB()
+database.insert(city)
+database.insert(country)
+
 # Print the average temperature of all the cities
 print("The average temperature of all the cities:")
 temps = []
 for city in cities:
     temps.append(float(city['temperature']))
 print(sum(temps)/len(temps))
+
+print(database.search('city').aggregate(lambda x: sum(x)/len(x),'temperature'))
 print()
 
 # Print all cities in Italy
@@ -31,6 +79,8 @@ for city in cities:
         temps.append(city['city'])
 print("All the cities in", my_country, ":")
 print(temps)
+
+print(database.search('city').filter(lambda x: x['country'] == 'Italy'))
 print()
 
 # Print the average temperature for all the cities in Italy
@@ -42,6 +92,8 @@ for city in cities:
         temps.append(float(city['temperature']))
 print("The average temperature of all the cities in", my_country, ":")
 print(sum(temps)/len(temps))
+
+print(database.search('city').filter(lambda x: x['country'] == 'Italy').aggregate(lambda x: sum(x)/len(x),'temperature'))
 print()
 
 # Print the max temperature for all the cities in Italy
@@ -53,6 +105,8 @@ for city in cities:
         temps.append(float(city['temperature']))
 print("The max temperature of all the cities in", my_country, ":")
 print(max(temps))
+
+print(database.search('city').filter(lambda x: x['country'] == 'Italy').aggregate(lambda x: max(x),'temperature'))
 print()
 
 # Let's write a function to filter out only items that meet the condition
@@ -74,9 +128,17 @@ def aggregate(aggregation_key, aggregation_function, dict_list):
 # Let's write code to
 # - print the average temperature for all the cities in Italy
 print(aggregate('temperature',lambda x: sum(x)/len(x),(filter(lambda x: x['country'] == 'Italy', cities))))
+
+print(database.search('city').filter(lambda x: x['country'] == 'Italy').aggregate(lambda x: sum(x)/len(x),'temperature'))
 # - print the average temperature for all the cities in Sweden
 print(aggregate('temperature',lambda x: sum(x)/len(x),(filter(lambda x: x['country'] == 'Sweden', cities))))
+
+print(database.search('city').filter(lambda x: x['country'] == 'Sweden').aggregate(lambda x: sum(x)/len(x),'temperature'))
 # - print the min temperature for all the cities in Italy
 print(aggregate('temperature',lambda x: min(x),(filter(lambda x: x['country'] == 'Italy', cities))))
+
+print(database.search('city').filter(lambda x: x['country'] == 'Italy').aggregate(lambda x: min(x),'temperature'))
 # - print the max temperature for all the cities in Sweden
 print(aggregate('temperature',lambda x: max(x),(filter(lambda x: x['country'] == 'Sweden', cities))))
+
+print(database.search('city').filter(lambda x: x['country'] == 'Sweden').aggregate(lambda x: max(x),'temperature'))
